4a7d5ceeaf14a6cf27bb72191b3abcb4648f8830
JM9K <leninj@student.okaloosaschools.com>
2026-02-23 17:42:48 +0000

    hehe

--- content.py
+++ content.py
@@ -6,21 +6,21 @@ if __name__ == "__main__":
 
 class questions():
     class multi_choice():
-        class test_question():
+        def test_question():
             question = "Test Qestion"
             wrong_answers = ["Wrong Answer 1", "Wrong Answer 2", "Wrong Answer 3"]
             correct_answer = "Correct Answer"
         class normal_questions():
-            class question1():
+            def question1():
                 question = "What is an example of Unsupervised learning? Filtering Emails, Diagnosing conditions, Video Game Recommendation, or Loan Approval prediction?"
                 wrong_answers = ["Filtering emails","Diagnosing conditions","Loan Approval prediction"]
                 correct_answer = "Video Game Recommendation"
         class trick_questions():
-            class question1():
+            def question1():
                 question = "What year was the first turing test? 1961, 1965, 1969, or 1963?"
                 wrong_answers = ["1961","1965","1969","1963"]
                 correct_answer = "1950"
     class written_answer():
-        class test_question():
+        def test_question():
             question = ""
             answers = ["","",""]
