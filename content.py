1205286cb7b3747f8f0283c7fa4b9a437956dab2
Ryder <fennera@student.okaloosaschools.com>
2026-02-23 10:05:39 -0600

    make test question

---
+++ content.py
@@ -0,0 +1,12 @@
+if __name__ == "__main__":
+    print("""Why are you running this?
+          This is the content script.
+          dummy""")
+    input("Press [Enter] to exit.")
+
+
+class test():
+    class test_question():
+        question = "Test Qestion"
+        wrong_answers = ["Wrong Answer 1", "Wrong Answer 2", "Wrong Answer 3"]
+        correct_answer = "Correct Answer"
