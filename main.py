9d411689ed4d35cae4b6053ba334c4e83806bcae
Aquanical254 <Aquanical254@hotmail.com>
2026-02-23 10:12:39 -0600

    added start

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 import time
+name = ""
+score = 0
 win = """
+|||        |||   |||||||||||   ||          ||           |||          |||  ||||||||||   ||||||         |||
+|||        |||   |         |   ||          ||           |||          |||     |||       ||||||         |||
+ |||      |||    |         |   ||          ||           |||          |||     |||       ||| |||        |||
+  |||    |||     |         |   ||          ||           |||          |||     |||       |||  |||       |||
+   |||  |||      |         |   ||          ||           |||          |||     |||       |||   |||      |||
+    ||||||       |         |   ||          ||           |||          |||     |||       |||    |||     |||
+      |||        |         |   ||          ||           |||  ||||||  |||     |||       |||     |||    |||
+      |||        |         |   ||          ||           ||| |||  ||| |||     |||       |||      |||   |||
+      |||        |         |    ||        ||             |||||    |||||      |||       |||       ||||||||
+      |||        |||||||||||     ||||||||||               |||      |||    ||||||||||   |||        |||||||"""
 
-|||        |||   |||||||||||
-|||        |||   |         |
- |||      |||    |         |
-  |||    |||     |         |
-   |||  |||      |         |
-    ||||||       |         |
-      |||        |         |
-      |||        |         |
-      |||        |         |
-      |||        |||||||||||"""
+def start():
+    global score, name
+    print("Hello and welcome to the AI study thing...")
+    time.sleep(1)
+    print("If you answer a question wrong, you fail an restart. This is on Section 1.1 - AI Fundamentals")
+    time.sleep(3)
+    name = input("What is your name, Studying person?")
+    if name == "Professor":
+        print("Hard mode activated... good luck!")
