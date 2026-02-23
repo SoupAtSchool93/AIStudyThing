0665f11956773390bd2db29d864cce3400424bfe
JM9K <leninj@student.okaloosaschools.com>
2026-02-23 16:09:48 +0000

    updated structure

--- content.py
+++ content.py
@@ -4,9 +4,23 @@ if __name__ == "__main__":
           dummy""")
     input("Press [Enter] to exit.")
 
-
-class test():
-    class test_question():
-        question = "Test Qestion"
-        wrong_answers = ["Wrong Answer 1", "Wrong Answer 2", "Wrong Answer 3"]
-        correct_answer = "Correct Answer"
+class questions():
+    class multi_choice():
+        class test_question():
+            question = "Test Qestion"
+            wrong_answers = ["Wrong Answer 1", "Wrong Answer 2", "Wrong Answer 3"]
+            correct_answer = "Correct Answer"
+        class normal_questions():
+            class question1():
+                question = ""
+                wrong_answers = ["","",""]
+                correct_answer = ""
+        class trick_questions():
+            class question1():
+                question = ""
+                wrong_answers = ["","",""]
+                correct_answer = ""
+    class written_answer():
+        class test_question():
+            question = ""
+            answers = ["","",""]
