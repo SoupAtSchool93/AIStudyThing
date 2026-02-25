a7076da0d12c1c8bc1914f319ea50ed1fa07c459
Ryder <fennera@student.okaloosaschools.com>
2026-02-25 15:44:11 +0000

    make written more obvious i guess?

--- content.py
+++ content.py
@@ -1,11 +1,11 @@
-if __name__ == "__main__":
+if __name__ == "__main__": #make sure this is not run directly
     print("""   Why are you running this?
    This is the content script.
    dummy""")
     input("Press [Enter] to exit.")
 
-class questions():
-    class multi_choice():
+class questions(): #the actual content
+    class multi_choice(): #multiple choice stuff
         test_question = {
             "question":"Test Question",
             "wrong_answers":["Wrong Answer 1", "Wrong Answer 2", "Wrong Answer 3"],
@@ -28,8 +28,13 @@ class questions():
                 "wrong_answers": ["1961","1965","1969","1963"],
                 "correct_answer": "1950"
             }
-    class written_answer():
+    class written_answer(): #written answer stuff
         test_question = {
             "question":"Test Question, answer is test",
             "correct_answer":"test",
         }
+        class normal_questions():
+            question1 = {
+            "question":"question 1, to be implemented",
+            "correct_answer":"answer",
+            }
